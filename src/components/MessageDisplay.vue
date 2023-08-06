1212fc11e349b6be5f9eab5f9ca3cc2de233554f
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2023-08-06 15:10:23 +0500

    create a message display component

---
+++ src/components/MessageDisplay.vue
@@ -0,0 +1,19 @@
+<script>
+import { getMessage } from "@/services/axios";
+
+export default {
+  data() {
+    return {
+      message: {},
+      error: null,
+    };
+  },
+  async created() {
+    try {
+      this.message = await getMessage();
+    } catch (err) {
+      this.error = "Oops! Something went wrong.";
+    }
+  },
+};
+</script>

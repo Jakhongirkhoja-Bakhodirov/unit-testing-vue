ce4b6eb9ca84eb6bda27c72d5b11ea563770b06a
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2023-08-19 23:14:21 +0500

    create message-container component

---
+++ src/components/MessageContainer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <MessageDisplay></MessageDisplay>
+  </div>
+</template>
+
+<script>
+import MessageDisplay from "./MessageDisplay.vue";
+export default {
+  components: MessageDisplay,
+};
+</script>
